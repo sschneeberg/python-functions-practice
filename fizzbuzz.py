185fa89317f51b681033f8f6794449d5849244e5
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-07 15:54:00 -0500

    fizzbuzz solution

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+"""
+Write a function called `fizzbuzz` that has a parameter `n`. `fizzbuzz` takes a numerical argument for the value of `n`, which is the maximum value to print up to. For example, if I gave you the number 10, I would expect your program to start at 1 and print up to 10.
+
+Additionally, your function should do the following:
+
+* If a number is divisible by 3, print `fizz`
+* If a number is divisible by 5, print `buzz`
+* If a number is divisible by 3 AND 5, print `fizzbuzz`
+* If none of the above are true, just print the number itself
+"""
+
+def fizzbuzz(n):
+    for m in range(1, n+1):
+        if (m % 3 == 0 and m % 5 == 0): print('fizzbuzz')
+        elif (m % 3 == 0): print('fizz')
+        elif (m % 5 == 0): print('buzz')
+        else: print(m)
+    return
+
+fizzbuzz(20)
